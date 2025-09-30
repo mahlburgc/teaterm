34fd10ecbe176c6b3a868e6d47df5f2fa8047250
cmahlburg <christian.mahlburg@nextbike.com>
2025-09-30 20:36:19 +0200

    add simple serial terminal

---
+++ readme.md
@@ -0,0 +1,8 @@
+# teaterm
+
+WIP
+
+Contains a serial terminal written in go.
+The idea is to have a serial terminal in go using the bubbletea framework. The framework should provide a nice
+interface for interacting with a serial interface. Also there will be a focus on implementing the ability to
+create predifined user commands.
---
+++ teaterm.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"time"
+
+	"go.bug.st/serial"
+	"go.bug.st/serial/enumerator"
+)
+
+// ReadFromPort should be called as go routine.
+// The function is intended to run continuously
+// and read the input stream from a serial port.
+func ReadFromPort(port serial.Port) {
+	scanner := bufio.NewScanner(port)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Printf("RX: %s\n", line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		// io.EOF is a "normal" error when the port is closed.
+		if err != io.EOF {
+			log.Printf("Error reading from serial port: %v", err)
+		}
+	}
+}
+
+// WriteToPort should be called as go routine.
+// The function is intended to run continuously, read from
+// user input stream and write the user input to the serial port.
+func WriteToPort(port serial.Port) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		userInput := scanner.Text()
+		stringToSend := userInput + "\r\n" // append line ending, TODO make configurable
+
+		_, err := port.Write([]byte(stringToSend))
+		if err != nil {
+			log.Printf("Error writing to serial port: %v", err)
+			return // TODO what to do on error, e.g. if the serial port is closed
+		} else {
+			fmt.Printf("TX: %s\n", userInput)
+		}
+	}
+
+	// This part is reached if scanner.Scan() returns false, usually on an error or EOF.
+	if err := scanner.Err(); err != nil {
+		log.Printf("Error reading from user input: %v", err)
+	}
+}
+
+// Main routine opens the serial port and starts the transmit
+// and receive go routines.
+func main() {
+
+	listPtr := flag.Bool("l", false, "list available ports")
+	portPtr := flag.String("p", "/dev/ttyUSB0", "serial port")
+
+	flag.Parse()
+
+	listFlag := *listPtr
+	// fmt.Println("list:", listFlag)
+	// fmt.Println("port:", *portPtr)
+
+	ports, err := enumerator.GetDetailedPortsList()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(ports) == 0 {
+		fmt.Println("No serial ports found!")
+		return
+	}
+
+	if listFlag {
+		for _, port := range ports {
+			fmt.Printf("Found port: %s\n", port.Name)
+			if port.IsUSB {
+				fmt.Printf("   USB ID     %s:%s\n", port.VID, port.PID)
+				fmt.Printf("   USB serial %s\n", port.SerialNumber)
+			}
+		}
+		return
+	}
+
+	mode := &serial.Mode{
+		BaudRate: 115200,
+	}
+	port, err := serial.Open(*portPtr, mode)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer port.Close()
+
+	fmt.Println("Start send and receive go rountine.")
+	go ReadFromPort(port)
+	go WriteToPort(port)
+
+	for {
+		time.Sleep(2 * time.Second)
+	}
+}
